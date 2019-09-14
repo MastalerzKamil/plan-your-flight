802b49085894f03711660cd81e524b476ac1fa95
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-09-15 01:46:47 +0200

    Add Carousel

--- frontend/src/components/Filters/index.js
+++ frontend/src/components/Filters/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Input } from 'semantic-ui-react'
+import { Input, Button } from 'semantic-ui-react'
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
-function Filters({ actions }) {
+function Filters({ actions, flightFilter, dateFilter }) {
   return (
     <div className='Filters'>
-      <div className='Filters__input'>
-        <Input focus onChange={(e) => actions.setStartDate(e.target.value)} placeholder='Od' />
+      <div className='Filters__row'>
+        <Input className='Filters__flightInput' focus onChange={(e) => actions.setfromFlight(e.target.value)} placeholder='Skąd lecimy' />
+        <Input className='Filters__flightInput' focus onChange={(e) => actions.setToFlight(e.target.value)} placeholder='Dokąd lecimy' />
+      </div>
+
+      <div className='Filters__row'>
+        <Input className='Filters__dateInput' focus onChange={(e) => actions.setStartDate(e.target.value)} placeholder='Od kiedy' />
+        <Input className='Filters__dateInput' focus onChange={(e) => actions.setEndDate(e.target.value)} placeholder='Do kiedy' />
+        <Button className='Filters__confirm' color='red'>Zatwierdź</Button>
       </div>
     </div>
   );
