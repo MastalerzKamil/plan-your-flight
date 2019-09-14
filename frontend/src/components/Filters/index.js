e723515d648bf44b943c1a0de3f7230325161730
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-09-14 20:17:12 +0200

    Refactor for Home

---
+++ frontend/src/components/Filters/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function Filters() {
+  return (
+    <div className='Filters'>
+      
+    </div>
+  );
+}
+
+export default Filters;
---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import Filters from 'components/Filters';
+
+function Home() {
+  return (
+    <div className='Root'>
+      <Filters />
+      elsdfsdk
+    </div>
+  );
+}
+
+export default Home;
