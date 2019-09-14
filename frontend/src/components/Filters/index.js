9c3dac4ad5a01fbf392ff8768e25dc9a7d32bf8a
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-09-14 20:46:12 +0200

    Remove fucking linter

--- frontend/src/components/Filters/index.js
+++ frontend/src/components/Filters/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
+
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 
 function Filters() {
   return (
     <div className='Filters'>
-      
+      <Button>Click Here</Button>
     </div>
   );
 }
