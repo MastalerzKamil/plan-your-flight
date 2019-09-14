16f89d92bdfcca77e397fcf7e4ba07a26d4bd4af
Tomasz Dłuski <mr.toumash@gmail.com>
2019-09-14 19:44:15 +0200

    adds working /flights

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,50 @@
 const express = require("express");
+const request = require("request");
+
 const app = express();
 const port = 3000;
 
-app.get("/flights", (req, res) => {
-  res.send("Hello World!");
+const getToken = () => {
+  var options = {
+    method: "POST",
+    url: "https://api.lot.com/flights-dev/v2/auth/token/get",
+    headers: {
+      "content-type": "application/json",
+      "x-api-key": "9YFNNKS31u9gCFKPetPWdAAjEXnED0B3K18AeYgg"
+    },
+    body: { secret_key: "2przp49a52" },
+    json: true
+  };
+  new Promise((res, rej) =>
+    request(options, function(error, response, body) {
+      if (error) throw new Error(error);
+
+      res(body.access_token);
+    })
+  );
+};
+const getAirports = async () => {
+  let token = await getToken();
+  var options = {
+    method: "GET",
+    url: "https://api.lot.com/flights-dev/v2/common/airports/get",
+    headers: {
+      "x-api-key": "9YFNNKS31u9gCFKPetPWdAAjEXnED0B3K18AeYgg",
+      authorization: "Bearer " + token
+    }
+  };
+  return new Promise((res, rej) =>
+    request(options, function(error, response, body) {
+      if (error) throw new Error(error);
+
+      res(body);
+    })
+  );
+};
+
+app.get("/airport", async (req, res) => {
+  let response = await getAirports();
+  res.send(response);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
