eee43bf7ab297369fc11863120cd8d9e7efdde89
Tomasz Dłuski <mr.toumash@gmail.com>
2019-09-14 17:06:16 +0200

    removes boilerplate code and makes folders

---
+++ plan-your-flight/src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function MainPage() {
+  return (
+    <div className="Root">
+      <text>Yo, yo</text>
+    </div>
+  );
+}
+
+export default MainPage;
