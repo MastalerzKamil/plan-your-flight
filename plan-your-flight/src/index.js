3f5330b6cc81e12c834d81a3f8708b65bfb08d98
Tomasz Dłuski <mr.toumash@gmail.com>
2019-09-14 17:06:16 +0200

    removes boilerplate code and makes folders

--- plan-your-flight/src/index.js
+++ plan-your-flight/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MainPage from './pages/MainPage';
 import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <Route path="/" component={MainPage} />
+        {/* <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} /> */}
+      </div>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
