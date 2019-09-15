d683aca157c8128f6f022bd12038571a0caea0de
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-09-15 07:46:46 +0200

    Repair inputs

--- frontend/src/components/Filters/index.js
+++ frontend/src/components/Filters/index.js
@@ -1,37 +1,47 @@
 import React from 'react';
-import { Input, Button } from 'semantic-ui-react'
+import { Input, Button, Checkbox, Dropdown } from 'semantic-ui-react'
 import { DateInput } from 'semantic-ui-calendar-react';
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
+const airports = [
+  { key: 'WAW', value: 'Warszawa', text: 'Warszawa (WAW)' },
+  { key: 'GDA', value: 'Gdańsk', text: 'Gdańsk (GDA)' },
+  { key: 'JFK', value: 'Nowy Jork', text: 'Nowy Jork (JFK)' },
+  { key: 'MIA', value: 'Miami', text: 'Miami (MIA)' }
+];
+
 function Filters({ actions, flightFilter, startDate, endDate }) {
   return (
     <div className='Filters'>
       <div className='Filters__row'>
-        <Input className='Filters__flightInput' focus onChange={(e) => actions.setfromFlight(e.target.value)} placeholder='Skąd lecimy' />
-        <Input className='Filters__flightInput' focus onChange={(e) => actions.setToFlight(e.target.value)} placeholder='Dokąd lecimy' />
+        <Dropdown className='Filters__flightInput' onChange={(e) => actions.setFromFlight(e.target.value)} placeholder='Skąd lecimy' search selection options={airports} />
+        <Dropdown className='Filters__flightInput' onChange={(e) => actions.setToFlight(e.target.value)} placeholder='Dokąd lecimy' search selection options={airports} />
       </div>
 
       <div className='Filters__row'>
-        <div className='Filters__dates'>
-          <DateInput
-            className='Filters__dateInput'
-            name="fromDate"
-            placeholder="Od kiedy"
-            value={startDate}
-            iconPosition="right"
-            onChange={(e, {name, value}) => actions.setStartDate(value)}
-          />
-          <DateInput
-            className='Filters__dateInput'
-            name="toDate"
-            placeholder="Od kiedy"
-            value={endDate}
-            iconPosition="right"
-            onChange={(e, {name, value}) => actions.setEndDate(value)}
-          />
-        </div>
+        <Checkbox label='Bilet w 2 strony' />
+        <Checkbox label='Bilet w jedną stronę' />
+      </div>
+
+      <div className='Filters__row'>
+        <DateInput
+          className='Filters__dateInput'
+          name="fromDate"
+          placeholder="Od kiedy"
+          value={startDate}
+          iconPosition="right"
+          onChange={(e, {name, value}) => actions.setStartDate(value)}
+        />
+        <DateInput
+          className='Filters__dateInput'
+          name="toDate"
+          placeholder="Od kiedy"
+          value={endDate}
+          iconPosition="right"
+          onChange={(e, {name, value}) => actions.setEndDate(value)}
+        />
         <Button className='Filters__confirm' color='red'>Zatwierdź</Button>
       </div>
     </div>
