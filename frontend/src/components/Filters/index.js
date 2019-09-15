a143a070a2dcc9c9ce82481691bd114910ef1de0
Tomasz Dłuski <mr.toumash@gmail.com>
2019-09-15 09:15:12 +0200

    u[dates filters styles

--- frontend/src/components/Filters/index.js
+++ frontend/src/components/Filters/index.js
@@ -1,66 +1,93 @@
-import React, { Component } from 'react';
-import { Input, Button } from 'semantic-ui-react'
-import { DateInput } from 'semantic-ui-calendar-react';
+import React, { Component } from "react";
+import { Input, Button, Grid } from "semantic-ui-react";
+import { DateInput } from "semantic-ui-calendar-react";
 
-import './index.css';
-import 'semantic-ui-css/semantic.min.css';
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
 
 class Filters extends Component {
   constructor() {
     super();
     this.state = {
-      startDate: '',
-      endDate: '',
-    }
+      startDate: "",
+      endDate: ""
+    };
   }
 
-  handleStartDate(e, {name, value}) {
+  handleStartDate(e, { name, value }) {
     const { actions } = this.props;
     if (this.state.hasOwnProperty(name)) {
       this.setState({ [name]: value });
     }
-    actions.setStartDate(value)
+    actions.setStartDate(value);
   }
 
-  handleEndDate(e, {name, value}) {
+  handleEndDate(e, { name, value }) {
     const { actions } = this.props;
     if (this.state.hasOwnProperty(name)) {
       this.setState({ [name]: value });
     }
-    actions.setEndDate(value)
+    actions.setEndDate(value);
   }
 
   render() {
     const { startDate, endDate } = this.state;
     const { actions } = this.props;
     return (
-      <div className='Filters'>
-        <div className='Filters__row'>
-          <Input className='Filters__flightInput' focus onChange={(e) => actions.setfromFlight(e.target.value)} placeholder='Skąd lecimy' />
-          <Input className='Filters__flightInput' focus onChange={(e) => actions.setToFlight(e.target.value)} placeholder='Dokąd lecimy' />
-        </div>
-  
-        <div className='Filters__row'>
-          <div className='Filters__dates'>
-            <DateInput
-              className='Filters__dateInput'
-              name="startDate"
-              placeholder="Od kiedy"
-              value={startDate}
-              iconPosition="right"
-              onChange={(e, {name, value}) => this.handleStartDate(e, {name, value})}
-            />
-            <DateInput
-              className='Filters__dateInput'
-              name="endDate"
-              placeholder="Od kiedy"
-              value={endDate}
-              iconPosition="right"
-              onChange={(e, {name, value}) => this.handleEndDate(e, {name, value})}
-            />
-          </div>
-          <Button className='Filters__confirm' color='red'>Zatwierdź</Button>
-        </div>
+      <div className="Filters">
+        <Grid className="Filters__grid">
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Input fluid
+                className="Filters__flightInput"
+                focus
+                onChange={e => actions.setfromFlight(e.target.value)}
+                placeholder="Skąd lecimy"
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Input fluid
+                className="Filters__flightInput"
+                focus
+                onChange={e => actions.setToFlight(e.target.value)}
+                placeholder="Dokąd lecimy"
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={3}>
+            <Grid.Column>
+              <DateInput
+                className="Filters__dateInput"
+                name="startDate"
+                placeholder="Od kiedy"
+                value={startDate}
+                iconPosition="right"
+                onChange={(e, { name, value }) =>
+                  this.handleStartDate(e, { name, value })
+                }
+                fluid
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <DateInput
+                className="Filters__dateInput"
+                name="endDate"
+                placeholder="Od kiedy"
+                value={endDate}
+                iconPosition="right"
+                onChange={(e, { name, value }) =>
+                  this.handleEndDate(e, { name, value })
+                }
+                fluid
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Button className="Filters__confirm" color="red" fluid>
+                Zatwierdź
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
